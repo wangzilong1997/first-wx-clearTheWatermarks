3b2bf7f336bc1608070993fb3defb7d05c115441
wangzilong1997 <1321787581@qq.com>
2022-01-17 14:08:29 +0800

    :art: 添加进度条

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,48 +1,76 @@
-// index.js
-// 获取应用实例
-const app = getApp()
-
+// pages/clear/index.js
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    // 输入的链接地址
+    inputVal:null,
+    // 预览的链接地址
+    prevVideo:null,
+    // 下载的链接地址
+    downloadUrl:null,
+    // 临时保存的视频路径
+    filePath:null,
+    progress:0,
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  onLoad(){
+    console.log('onload',this)
   },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
+  inputChange(e){
+    console.log(e)
+    this.setData({ inputVal: e.detail.value })
   },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
+  tapSure(e){
+    console.log(this)
+    console.log('tapSure确定按钮执行',this.data.inputVal)
+    wx.request({
+      url: 'https://www.guofudiyiqianduan.com/clearTheWater/getTKUrl/realUrl',
+      method:'POST',
+      header:{
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      data:{
+        urlStr:this.data.inputVal
+      },
+      success:(e)=>{
+        console.log(e)
         this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
+          prevVideo:e.data.stdout,
+          downloadUrl:e.data.fileName
+        })
+        const downloadTask =  wx.downloadFile({
+          url: e.data.fileName,
+          success: res =>{
+            this.setData({
+              filePath:res.tempFilePath
+            })
+          }
+        })
+        downloadTask.onProgressUpdate((res) => {
+          if (res.progress === 100) {
+            this.setData({
+              progress: res.progress
+            })
+          } else {
+            this.setData({
+              progress: res.progress
+            })
+          }
         })
       }
     })
   },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+  download(e){
+    wx.saveVideoToPhotosAlbum({
+      filePath: this.data.filePath,
+      success: res => {
+        console.log('保存到相册成功',res)
+      },
+      fail: res => {
+        console.log('保存到相册失败',res);
+      }
+    })  
   }
 })

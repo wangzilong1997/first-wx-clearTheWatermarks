0c0e21af9bee6029885a4fbbe00ea2480e1e5840
wangzilong1997 <1321787581@qq.com>
2022-09-26 14:39:17 +0800

    fix

--- pages/select/select.js
+++ pages/select/select.js
@@ -22,6 +22,7 @@ Page({
       url: `https://f.4dbim.vip/precast/api/wechat/welder/search?pid=637&access_token=kxjMrzOahMRy84KSRO-SrPbtFY8djGAvjj99n96C&Like=${like}`,
       success:(res) => {
         console.log('获取数据',res)
+        // callback 返回的数据必须是{ID:xxx,Name:xxx}
         callback(res.data.data.map(item => {
           item.Name = item.MachineID
           return item
